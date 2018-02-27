a88d85694c7174d01b145850988f4aa4d5441d99
Ika <innakuzlakova@gmail.com>
2018-02-27 11:10:04 +0600

    add lab1

---
+++ lab1/ConsoleApp4/NumberOperator.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace ConsoleApp4
+{
+    public class NumberOperator
+    {
+         public int CalculateMultiplication(int number)
+        {
+            int[] digits = ExtractDigits(number);
+            return digits.Aggregate((d, result) => d * result);
+        }
+        public int CalculateSum(int number)
+        {
+            int[] digits = ExtractDigits(number);
+            return digits.Sum();
+        }
+        private int[] ExtractDigits(int number)
+        {
+            int[] result = new int[4];
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = number % 10;
+                number /= 10;
+            }
+
+            return result;
+        }
+    }
+}
