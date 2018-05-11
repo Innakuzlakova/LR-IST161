fd5209616c3836907d71a95a86241fa63b88f208
Innakuzlakova <36181724+Innakuzlakova@users.noreply.github.com>
2018-05-11 20:20:20 +0600

    Add files via upload

---
+++ LR2/LR2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0; //сюда мы сохраним итоговое число 
+            Console.WriteLine("Введите четырёхзначное число:");
+            string s = Console.ReadLine();//объявление и считывание. 
+            a = Convert.ToInt32(s); //получение числа
+            NumberOperator Number = new NumberOperator();
+           
+            int mult = Number.CalculateMultiplication(a);
+           
+            int sum = Number.CalculateSum(a);
+            Console.WriteLine("Произведение чисел:");
+            Console.WriteLine(mult);
+            Console.WriteLine("Сумма чисел:");
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ LR2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ProjectTwo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("12345");
+            string input = Console.ReadLine();
+            Number numb = new Number();
+            int st = numb.Method(input);
+            Console.WriteLine("Результат: {0}", st);
+            Console.ReadKey();
+        }
+    }
+}
+
+
