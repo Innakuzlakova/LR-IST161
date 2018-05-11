6e6eaa4c90843e730957edc3209f20d1a2fcb28a
Innakuzlakova <36181724+Innakuzlakova@users.noreply.github.com>
2018-05-11 20:02:03 +0600

    Add files via upload

---
+++ LR10/ConsoleApp26/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lr10
+{
+    class Number
+    {
+        public int Method(string input)
+        {
+            int intResult;
+            double doubleResult;
+            int b;
+
+            if (Int32.TryParse(input, out intResult))
+            {
+                b = 1;
+
+            }
+            else if (Double.TryParse(input, out doubleResult))
+            {
+                b = 2;
+            }
+            else
+            {
+                b = 0;
+            }
+
+            return b;
+        }
+    }
+}
---
+++ LR10/ConsoleApp26/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace Lr10
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"D:\input.txt");
+            string input = sr.ReadLine();
+            Number numb = new Number();
+            int st = numb.Method(input);
+            Console.WriteLine("Результат: {0}", st);
+            StreamWriter sw = new StreamWriter(@"D:\output.txt");
+            sw.Write(st);
+
+            sw.Close();
+            Console.ReadKey();
+            Console.ReadLine();
+        }
+    }
+}
