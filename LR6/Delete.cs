9ee9b596b61aa39b863ca2f72f63fc1d26028c19
Innakuzlakova <36181724+Innakuzlakova@users.noreply.github.com>
2018-05-11 20:50:57 +0600

    Add files via upload

---
+++ LR6/Delete.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    class Delete
+    {
+        public int[] DeletingFromArray(int[] a, int n)
+        {
+            int m = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int k = 0;
+                m = a[i];
+                for (int j = 0; j < n; j++)
+                {
+                    if (a[j] == m)
+                    {
+                        k = k + 1;
+                    }
+                    if (k > 2)
+                        for (int l = 0; l < a.Length; l++)
+                        {
+                            if (a[l] == m)
+                                Array.Clear(a, l, 1);
+                        }
+                }
+            }
+
+            return a;
+        }
+    }
+}
---
+++ LR6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Rextester
+{
+    using ConsoleApp20;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int n;
+            Console.WriteLine("Введите размер массива ");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[n];
+            double m;
+            Console.WriteLine("Введите элементы массива:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("a[" + Convert.ToString(i + 1) + "]=");
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Delete del = new Delete();
+            int[] newArr = del.DeletingFromArray(a, n);
+                
+
+            for (int i = 0; i < newArr.Length; i++)
+                if (newArr[i] != 0)
+                    Console.Write(newArr[i]);
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
