29262d1b853ca1c02c485ce0996a06c52ed3d93a
Innakuzlakova <36181724+Innakuzlakova@users.noreply.github.com>
2018-05-11 20:38:33 +0600

    Add files via upload

---
+++ LR4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Numb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Изначальный : ");
+            int number = 15;
+            int[] array = new int[number];
+            Random rand = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(0, 10);
+                Console.Write(array[i] + " ");
+            }
+            Rearanger Transfor = new  Rearanger();
+            int[] result = Transfor.Rearange(array);
+            Console.WriteLine();
+            Console.WriteLine("Pезультат:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LR4/Rearanger.cs
@@ -0,0 +1,17 @@
+﻿namespace Numb
+{
+    class Rearanger
+    {
+        public int[] Rearange(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int variable = array[2];
+                array[2] = array[4];
+                array[4] = variable;
+            }
+
+            return array;
+        }
+    }
+}
