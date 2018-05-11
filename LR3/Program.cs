de00e7bf4b9c414776518ec2e7f53b49dfe366d1
Innakuzlakova <36181724+Innakuzlakova@users.noreply.github.com>
2018-05-11 20:32:13 +0600

    Add files via upload

---
+++ LR3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Reiteration art1 = new Reiteration();
+            Console.WriteLine();
+            int input = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[input];
+            Random ran = new Random();
+            for (int i = 0; i < input; i++)
+            {
+                array[i] = ran.Next(-10, 10);
+                Console.Write("{0} ", array[i]);
+
+            }
+            int result = art1.a(array);
+            Console.WriteLine();
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
