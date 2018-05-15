53f878bbe2cc7a1144e65fb196f1e8363e65cfa0
Innakuzlakova <36181724+Innakuzlakova@users.noreply.github.com>
2018-05-15 09:54:55 +0600

    Add files via upload

---
+++ ConsoleApp31/ConsoleApp31/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace lr9
+{
+    class Change
+    {
+        private int[,] arr;
+        private int[] newArray = new int[16];
+        public Change(int[,] firstArr)
+        {
+            arr = firstArr;
+        }
+
+        private int[] FormatinArr()
+        {
+            int k = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    newArray[k] = arr[i, j];
+                    k++;
+                }
+            }
+            Array.Sort(newArray);
+            return newArray;
+        }
+
+        //МЕНЯТЬ ЗДЕСЬ
+        public int[,] MadeNewArray()
+        {
+            FormatinArr();
+            int z = 0;
+            for (int i = 0; i < arr.GetLength(1); i++)
+
+            {
+                for (int j = arr.GetLength(0) - 1; j >= 0; --j)
+                {
+                    //Console.Write(arr [j,i] + " ");
+                    arr[i, j] = newArray[z];
+                    z++;
+
+                }
+
+            }
+
+            return arr;
+        }
+
+    }
+}
+
---
+++ ConsoleApp31/ConsoleApp31/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace lr9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] firstArr = new int[4, 4] { { 1, 6, 2, 10 }, { 4, 3, 5, 11 }, { 9, 7, 8, 12 }, { 13, 15, 14, 16 } };
+
+            Change newArray = new Change(firstArr);
+            int[,] newMassiv = newArray.MadeNewArray();
+            for (int i = 0; i < newMassiv.GetLength(0); i++)
+            {
+                for (int j = 0; j < newMassiv.GetLength(1); j++)
+                {
+                    Console.Write(newMassiv[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
+
